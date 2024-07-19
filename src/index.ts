38605bdd2cd71d9ed2c9f1502f4300d569465907
James Grugett <jahooma@gmail.com>
2024-07-19 18:02:23 +0800

    Menu

--- src/index.ts
+++ src/index.ts
@@ -121,6 +121,11 @@ async function manicode(userPrompt: string | undefined) {
   let inputBuffer = ''
   let historyIndex = -1
   const history: string[] = []
+  let isInMenu = false
+  let menuSelectedIndex = 0
+  let menuOffset = 0
+  const NEW_CHAT_OPTION = '+ New chat'
+  const CHATS_PER_PAGE = 5
 
   const clearLine = () => {
     process.stdout.clearLine(0)
@@ -132,10 +137,63 @@ async function manicode(userPrompt: string | undefined) {
     process.stdout.write(`> ${inputBuffer}`)
   }
 
+  const displayMenu = () => {
+    console.clear()
+    console.log('Chat History:')
+    const chats = chatStorage.listChats()
+    const totalItems = chats.length + 1 // +1 for the "New Chat" option
+    const startIndex = menuOffset
+    const endIndex = Math.min(startIndex + CHATS_PER_PAGE, totalItems)
+
+    for (let i = startIndex; i < endIndex; i++) {
+      if (i < chats.length) {
+        const chat = chats[i]
+        const isSelected = i === menuSelectedIndex
+        const marker = isSelected ? '>' : ' '
+        console.log(
+          `${marker} ${chat.id} (${new Date(chat.updatedAt).toLocaleString()})`
+        )
+      } else {
+        const isSelected = i === menuSelectedIndex
+        const marker = isSelected ? '>' : ' '
+        console.log(`${marker} ${NEW_CHAT_OPTION}`)
+      }
+    }
+
+    if (totalItems > CHATS_PER_PAGE) {
+      console.log(
+        `\nShowing ${startIndex + 1}-${endIndex} of ${totalItems} items`
+      )
+    }
+
+    console.log('\nUse arrow keys to navigate, SPACE to select, ESC to exit')
+  }
+  const initializeMenu = () => {
+    const chats = chatStorage.listChats()
+    const currentChatIndex = chats.findIndex(
+      (chat) => chat.id === currentChat.id
+    )
+
+    if (currentChatIndex !== -1) {
+      menuSelectedIndex = currentChatIndex
+      menuOffset = Math.max(
+        0,
+        Math.min(currentChatIndex, chats.length - CHATS_PER_PAGE + 1)
+      )
+    } else {
+      menuSelectedIndex = 0
+      menuOffset = 0
+    }
+
+    isInMenu = true
+    displayMenu()
+  }
+
   process.stdin.on('data', (key: string) => {
     const ESC_KEY = '\u001B'
     const ENTER_KEY = '\r'
     const BACKSPACE_KEY = '\x7F'
+    const SPACE_KEY = ' '
 
     if (key === ESC_KEY) {
       if (isReceivingResponse) {
@@ -144,9 +202,53 @@ async function manicode(userPrompt: string | undefined) {
         console.log('\n[Response stopped by user]')
         isReceivingResponse = false
         promptUser()
-      } else {
-        console.log('\nExiting. Manicode out!')
+      } else if (isInMenu) {
+        isInMenu = false
+        console.clear()
+        console.log('Exiting. Manicode out!')
         process.exit(0)
+      } else {
+        initializeMenu()
+      }
+    } else if (isInMenu) {
+      const chats = chatStorage.listChats()
+      const totalItems = chats.length + 1 // +1 for the "New Chat" option
+      if (key === '\u001B[A') {
+        // Up arrow
+        if (menuSelectedIndex > menuOffset) {
+          menuSelectedIndex--
+        } else if (menuOffset > 0) {
+          menuOffset--
+          menuSelectedIndex--
+        }
+        displayMenu()
+      } else if (key === '\u001B[B') {
+        // Down arrow
+        if (
+          menuSelectedIndex <
+          Math.min(menuOffset + CHATS_PER_PAGE - 1, totalItems - 1)
+        ) {
+          menuSelectedIndex++
+        } else if (menuOffset + CHATS_PER_PAGE < totalItems) {
+          menuOffset++
+          menuSelectedIndex++
+        }
+        displayMenu()
+      } else if (key === SPACE_KEY || key === ENTER_KEY) {
+        if (menuSelectedIndex < chats.length) {
+          currentChat = chats[menuSelectedIndex]
+          isInMenu = false
+          console.clear()
+          console.log(`Switched to chat: ${currentChat.id}`)
+          promptUser()
+        } else {
+          // Create a new chat
+          currentChat = chatStorage.createChat()
+          isInMenu = false
+          console.clear()
+          console.log(`Created new chat: ${currentChat.id}`)
+          promptUser()
+        }
       }
     } else if (key === ENTER_KEY) {
       console.log() // Move to the next line
@@ -164,13 +266,14 @@ async function manicode(userPrompt: string | undefined) {
         inputBuffer = inputBuffer.slice(0, -1)
         refreshLine()
       }
-    } else if (key === '\u001B[A' || key === '\u001B[B') { // Up or Down arrow
+    } else if (key === '\u001B[A' || key === '\u001B[B') {
+      // Up or Down arrow
       if (key === '\u001B[A' && historyIndex < history.length - 1) {
         historyIndex++
       } else if (key === '\u001B[B' && historyIndex > -1) {
         historyIndex--
       }
-      
+
       if (historyIndex === -1) {
         inputBuffer = ''
       } else {
