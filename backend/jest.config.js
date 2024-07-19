183a350c3b19c495d9d02fdb2dc70f02595c49bc
James Grugett <jahooma@gmail.com>
2024-07-19 12:38:11 +0800

    Remove jest warning

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,20 +1,18 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/*.test.ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: 'tsconfig.json'
+    }],
   },
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
   moduleNameMapper: {
     '^common(.*)$': '<rootDir>/../common/src$1',
   },
   moduleDirectories: ['node_modules', '../node_modules'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Add this line to include the setup file
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
