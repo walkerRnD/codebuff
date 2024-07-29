ac8f7b0b58fa530e57afb7acf99ed181d6d9fdbe
James Grugett <jahooma@gmail.com>
2024-07-29 11:11:59 -0700

    better menu instructions

--- npm-app/src/menu.ts
+++ npm-app/src/menu.ts
@@ -7,22 +7,24 @@ export function displayMenu() {
 | | | | | | (_| | | | | | (_| (_) | (_| |  __/
 |_| |_| |_|\\__,_|_| |_|_|\\___\\___/ \\__,_|\\___|
 `)
-  console.log('\nPress SPACE to chat, ESC to exit\n\n')
-  console.log('TODO')
+  console.log('\nPress ESC to close menu, CTRL-C to exit\n\n')
+  console.log('TIPS')
   console.log('')
   console.log(
     '() Create a knowledge.md file and write down your wisdom for Manny, your assistant.'
   )
   console.log('')
   console.log(
-    '() Ask Manny to update knowledge files when you\'ve told him how he\'s done something wrong.'
+    "() Ask Manny to update knowledge files when he's done something wrong."
   )
   console.log('')
   console.log(
-    '() Press the LEFT and RIGHT arrow keys to revert or reapply file changes from the conversation.'
+    '() Press the CTRL-U to undo file changes from the conversation. Press CTRL-R to redo.'
   )
   console.log('')
-  console.log('() Press ESC while Manny is generating a response to cut him off.')
+  console.log(
+    '() Press ESC while Manny is generating a response to cut him off.'
+  )
   console.log('')
   console.log(
     '() Email your feedback to james@manifold.markets. Thanks for using Manicode!'
