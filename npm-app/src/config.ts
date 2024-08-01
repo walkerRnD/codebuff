d69cdff9915466c788a54aca87caa3607805f791
James Grugett <jahooma@gmail.com>
2024-08-01 13:08:14 -0700

    Tweak

--- npm-app/src/config.ts
+++ npm-app/src/config.ts
@@ -9,14 +9,6 @@ function getCurrentDirectory() {
   try {
     return process.cwd()
   } catch (error) {
-    if (__dirname) {
-      console.warn(
-        'Failed to get current working directory. Is this directory deleted?',
-        error
-      )
-      console.warn('Using __dirname instead:', __dirname)
-      return __dirname
-    }
     throw new Error(
       'Failed to get current working directory. Is this directory deleted?',
       { cause: error }
