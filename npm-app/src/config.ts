3290a94ae8acd9903478784181f8da2f80b7b8fa
James Grugett <jahooma@gmail.com>
2024-08-01 12:55:21 -0700

    Catch cwd() failing.

--- npm-app/src/config.ts
+++ npm-app/src/config.ts
@@ -2,4 +2,24 @@ const isProduction = process.env.NODE_ENV === 'production'
 export const websocketUrl = isProduction
   ? 'ws://api.manicode.ai:4242/ws'
   : 'ws://localhost:4242/ws'
-export const projectRoot = process.cwd()
+
+export const projectRoot = getCurrentDirectory()
+
+function getCurrentDirectory() {
+  try {
+    return process.cwd()
+  } catch (error) {
+    if (__dirname) {
+      console.warn(
+        'Failed to get current working directory. Is this directory deleted?',
+        error
+      )
+      console.warn('Using __dirname instead:', __dirname)
+      return __dirname
+    }
+    throw new Error(
+      'Failed to get current working directory. Is this directory deleted?',
+      { cause: error }
+    )
+  }
+}
