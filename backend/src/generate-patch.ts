0cebd4bf70444e7352608f697ac753c90479bf34
James Grugett <jahooma@gmail.com>
2024-09-18 21:13:23 -0700

    Check if sketch has no comments and forgo patch

--- backend/src/generate-patch.ts
+++ backend/src/generate-patch.ts
@@ -1,6 +1,7 @@
 import { Message } from 'common/actions'
 import { promptOpenAI } from './openai-api'
 import { debugLog } from './util/debug'
+import { createPatch } from 'diff'
 
 export async function generatePatch(
   userId: string,
@@ -15,7 +16,8 @@ export async function generatePatch(
   const normalizedOldContent = normalizeLineEndings(oldContent)
   const normalizedNewContent = normalizeLineEndings(newContent)
 
-  const patch = await generatePatchPrompt(
+  let patch = ''
+  const isSketchComplete = await isSketchCompletePrompt(
     userId,
     normalizedOldContent,
     normalizedNewContent,
@@ -23,9 +25,64 @@ export async function generatePatch(
     messageHistory,
     fullResponse
   )
+  if (isSketchComplete) {
+    patch = createPatch(filePath, normalizedOldContent, normalizedNewContent)
+    const lines = patch.split('\n')
+    const hunkStartIndex = lines.findIndex((line) => line.startsWith('@@'))
+    if (hunkStartIndex !== -1) {
+      patch = lines.slice(hunkStartIndex).join('\n')
+    }
+  } else {
+    patch = await generatePatchPrompt(
+      userId,
+      normalizedOldContent,
+      normalizedNewContent,
+      filePath,
+      messageHistory,
+      fullResponse
+    )
+  }
   debugLog('got patch', newContent, '\n\n', patch)
   const updatedPatch = patch.replaceAll('\n', lineEnding)
   return updatedPatch
+}
+
+const isSketchCompletePrompt = async (
+  userId: string,
+  oldContent: string,
+  newContent: string,
+  filePath: string,
+  messageHistory: Message[],
+  fullResponse: string
+) => {
+  const prompt = `
+Here's the original file:
+
+\`\`\`
+${oldContent}
+\`\`\`
+
+And here's the new content with a sketch of the changes to be made. It may have placeholder comments that should be expanded into code:
+
+\`\`\`
+${newContent}
+\`\`\`
+
+Are there any comments in the sketch that indicate surrounding code should remain as it is in the original file? For example, comments like "// ... existing code ..." or "# .... rest of the function ...". If so, please write [YES]. Otherwise, write [NO].
+`.trim()
+
+  const messages = [
+    {
+      role: 'user' as const,
+      content: prompt,
+    },
+  ]
+  const response = await promptOpenAI(userId, messages, 'gpt-4o-2024-08-06')
+  const isSketchComplete = response.includes('NO')
+  debugLog(response)
+  debugLog('isSketchComplete', isSketchComplete)
+
+  return isSketchComplete
 }
 
 const generatePatchPrompt = async (
@@ -41,7 +98,7 @@ const generatePatchPrompt = async (
     .map((line, index) => `${index + 1}|${line}`)
     .join('\n')
   const prompt = `
-Here's an old file for ${filePath}:
+Here's an old file:
 
 \`\`\`
 ${oldFileWithLineNumbers}
