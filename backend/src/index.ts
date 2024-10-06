297837c6e47b10dca813e1858d08906bf724286a
James Grugett <jahooma@gmail.com>
2024-10-06 14:47:22 -0700

    Log uncaughException

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -41,3 +41,17 @@ webSocketListen(server, '/ws')
 process.on('SIGINT', () => {
   process.exit()
 })
+
+process.on('uncaughtException', (err) => {
+  logger.fatal(err, 'uncaught exception detected')
+  server.close(() => {
+    process.exit(1)
+  })
+
+  // If a graceful shutdown is not achieved after 1 second,
+  // shut down the process completely
+  setTimeout(() => {
+    process.abort() // exit immediately and generate a core dump file
+  }, 1000).unref()
+  process.exit(1)
+})
