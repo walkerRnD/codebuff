0e042041e3e12fa41a3cb7518454f73b1b482fd2
James Grugett <jahooma@gmail.com>
2024-10-22 11:17:19 -0700

    Increase the max websocket timeout

--- common/src/websockets/websocket-client.ts
+++ common/src/websockets/websocket-client.ts
@@ -11,10 +11,9 @@ import {
 // mqp: useful for debugging
 const VERBOSE_LOGGING = false
 
-// mqp: no way should our server ever take 5 seconds to reply
-const TIMEOUT_MS = 10000
+const TIMEOUT_MS = 30_000
 
-const RECONNECT_WAIT_MS = 5000
+const RECONNECT_WAIT_MS = 5_000
 
 type ConnectingState = typeof WebSocket.CONNECTING
 type OpenState = typeof WebSocket.OPEN
