0be6d5c775aa8b35f193183a1d44cb8bac87e4a7
Brandon Chen <brandonchenjiacheng@gmail.com>
2024-12-05 18:57:11 -0800

    fix: commented out stripe logging

--- common/src/util/stripe.ts
+++ common/src/util/stripe.ts
@@ -6,11 +6,11 @@ export const stripeServer = new Stripe(env.STRIPE_SECRET_KEY, {
   apiVersion: '2024-06-20',
 })
 
-stripeServer.on('request', (request) => {
-  console.log('Stripe API Request', request)
-})
+// stripeServer.on('request', (request) => {
+//   console.log('Stripe API Request', request)
+// })
 
-// Listen for response events
-stripeServer.on('response', (response) => {
-  console.log('Stripe API Response', response)
-})
+// // Listen for response events
+// stripeServer.on('response', (response) => {
+//   console.log('Stripe API Response', response)
+// })
