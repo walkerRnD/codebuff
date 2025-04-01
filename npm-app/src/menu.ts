ffaf0b4aeb386f1729a8ccb14d02f8d5cf33645d
brandonkachen <brandonchenjiacheng@gmail.com>
2025-03-31 18:34:20 -0700

    feat: 4/1 🃏

--- npm-app/src/menu.ts
+++ npm-app/src/menu.ts
@@ -48,7 +48,7 @@ export function displayGreeting(costMode: CostMode, username: string | null) {
   }
 
   console.log(
-    `\nWelcome${username ? ` back ${username}` : ''}! What would you like to do?`
+    `\nWelcome${username ? ` back ${username}` : ''}! What would you like to do? 🃏`
   )
 }
 
