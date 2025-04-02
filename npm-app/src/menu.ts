dc526a355e42dcc204a0cf1884fdb9f6aa96f7ea
brandonkachen <brandonchenjiacheng@gmail.com>
2025-04-02 00:59:44 -0700

    feat: remove 🃏 emoji

--- npm-app/src/menu.ts
+++ npm-app/src/menu.ts
@@ -48,7 +48,7 @@ export function displayGreeting(costMode: CostMode, username: string | null) {
   }
 
   console.log(
-    `\nWelcome${username ? ` back ${username}` : ''}! What would you like to do? 🃏`
+    `\nWelcome${username ? ` back ${username}` : ''}! What would you like to do?`
   )
 }
 
