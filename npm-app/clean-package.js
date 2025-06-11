2fdaf7d8d9543f7bde4fc44bf367af5a54530958
brandonkachen <brandonchenjiacheng@gmail.com>
2025-06-11 12:52:20 -0700

    fix: npm-app deploy

--- npm-app/clean-package.js
+++ npm-app/clean-package.js
@@ -1,9 +1,6 @@
 const fs = require('fs')
 const path = require('path')
 
-// Set production environment for the build
-process.env.NEXT_PUBLIC_CB_ENVIRONMENT = 'prod'
-
 const packageJsonPath = path.join(__dirname, 'package.json')
 const tempPackageJsonPath = path.join(__dirname, 'temp.package.json')
 const indexJsPath = path.join(__dirname, 'dist', 'index.js')
@@ -22,12 +19,8 @@ for (const depType of ['dependencies', 'optionalDependencies']) {
       if (typeof version === 'string' && version.startsWith('workspace:')) {
         if (pkgName === 'common') {
           delete packageJson[depType][pkgName]
-          console.log(
-            `Removed dependency on ${pkgName} because it's now bundled.`
-          )
         } else {
           packageJson[depType][pkgName] = '1.0.0'
-          console.warn(`No version found for ${pkgName}, defaulting to 1.0.0`)
         }
       }
     }
@@ -44,11 +37,25 @@ fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2))
 if (fs.existsSync(indexJsPath)) {
   let indexJsContent = fs.readFileSync(indexJsPath, 'utf8')
 
+  // Dynamically get client-side environment variables from env.ts
+  const envTsContent = fs.readFileSync(
+    path.join(__dirname, '..', 'env.ts'),
+    'utf8'
+  )
+  const clientKeysMatch = envTsContent.match(/client: {([^]*?)}/s)
+  let clientKeys = []
+  if (clientKeysMatch) {
+    clientKeys =
+      clientKeysMatch[1]
+        .split('\n')
+        .map((line) => line.match(/^\s*(\w+):/))
+        .filter(Boolean)
+        .map((match) => match[1]) ?? []
+  }
+
   // Get all environment variables that start with NEXT_PUBLIC_ or are needed for the npm package
   const envVarsToInject = Object.entries(process.env)
-    .filter(
-      ([key, value]) => key.startsWith('NEXT_PUBLIC_') && value !== undefined
-    )
+    .filter(([key, value]) => clientKeys.includes(key) && value !== undefined)
     .map(([key, value]) => `process.env.${key} = '${value}';`)
 
   const lines = indexJsContent.split('\n')
@@ -57,12 +64,9 @@ if (fs.existsSync(indexJsPath)) {
   indexJsContent = lines.join('\n')
   fs.writeFileSync(indexJsPath, indexJsContent)
   console.log(
-    `Injected ${envVarsToInject.length} environment variables into index.js`
+    `Injected ${envVarsToInject.length} environment variables into index.js:`
   )
+  envVarsToInject.forEach((envVar) => console.log(`- ${envVar}`))
 } else {
   console.error('index.js not found in the dist directory')
 }
-
-console.log(
-  'package.json has been cleaned for publishing and index.js has been updated.'
-)
