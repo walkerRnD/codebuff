dec8918f579c23b6766ef3d1704d8ecf8e6336bc
James Grugett <jahooma@gmail.com>
2025-07-08 00:04:48 -0700

    Delete jest config

--- backend/jest.setup.js
+++
@@ -1,9 +0,0 @@
-
-const dotenv = require('dotenv');
-const path = require('path');
-
-// Load environment variables from .env file
-dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-// You can add more setup code here if needed in the future
-console.log('Jest setup: Environment variables loaded');
