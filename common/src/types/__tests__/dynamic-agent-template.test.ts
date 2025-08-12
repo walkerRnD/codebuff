b1a9f84f819b4cc3adb97bd5aaa0d7535045b442
James Grugett <jahooma@gmail.com>
2025-08-11 18:47:07 -0700

    Fix types

--- common/src/types/__tests__/dynamic-agent-template.test.ts
+++ common/src/types/__tests__/dynamic-agent-template.test.ts
@@ -1,10 +1,15 @@
+import { publishedTools } from 'src/tools/list'
 import type { AgentDefinition } from '../../util/types/agent-definition'
 import type { DynamicAgentDefinition } from '../dynamic-agent-template'
 
 // Create a version of DynamicAgentDefinition where handleSteps is compatible with AgentDefinition
 
-type DynamicAgentDefinitionHandleSteps = Omit<DynamicAgentDefinition, 'handleSteps'> & {
+type DynamicAgentDefinitionHandleSteps = Omit<
+  DynamicAgentDefinition,
+  'handleSteps' | 'toolNames'
+> & {
   handleSteps?: AgentDefinition['handleSteps']
+  toolNames?: (typeof publishedTools)[number][]
 }
 // Don't remove these lines! And don't change the values away from true!
 const _typecheck1: AgentDefinition extends DynamicAgentDefinitionHandleSteps
@@ -14,7 +19,8 @@ const _typecheck2: DynamicAgentDefinitionHandleSteps extends AgentDefinition
   ? true
   : false = true
 // These two give nicer to read type errors. Let's keep them.
-const a: DynamicAgentDefinitionHandleSteps = {} as DynamicAgentDefinitionHandleSteps
+const a: DynamicAgentDefinitionHandleSteps =
+  {} as DynamicAgentDefinitionHandleSteps
 const b: AgentDefinition = {} as DynamicAgentDefinitionHandleSteps
 const _keyTypecheck1: keyof AgentDefinition =
   {} as keyof DynamicAgentDefinitionHandleSteps
