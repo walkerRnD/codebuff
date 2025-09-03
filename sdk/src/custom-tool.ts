f472564bc5204e8d4cf6ed3517167d099b7bc480
James Grugett <jahooma@gmail.com>
2025-09-03 14:41:33 -0700

    Fix import in sdk

--- sdk/src/custom-tool.ts
+++ sdk/src/custom-tool.ts
@@ -1,6 +1,6 @@
 import z from 'zod/v4'
 
-import type { ToolResultOutput } from '@codebuff/common/types/messages/content-part'
+import type { ToolResultOutput } from '../../common/src/types/messages/content-part'
 import type { JSONSchema } from 'zod/v4/core'
 
 export type CustomToolDefinition<
