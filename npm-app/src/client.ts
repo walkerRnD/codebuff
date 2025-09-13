c4c5cf6b5571fc6e3b7e5f83bf681eba4792d0d6
James Grugett <jahooma@gmail.com>
2025-09-13 15:48:05 -0700

    Show spinner after reading files

--- npm-app/src/client.ts
+++ npm-app/src/client.ts
@@ -785,6 +785,9 @@ export class Client {
         files,
         requestId,
       })
+        if (this.userInputId) {
+          Spinner.get().start('Reading through files...')
+        }
     })
 
     // Handle backend-initiated tool call requests
